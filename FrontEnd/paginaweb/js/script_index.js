952ac8f0aaaba86cf2e76609c3711d2e9d702b31
Alexandre Vieira <alxndr331@gmail.com>
2022-09-23 03:58:56 -0300

    aaaaahhhhhhhhhh quero trancar o cursooo, fiz muita coisa foda, slk 4 da madrugada kkkkkk vo acordar 5:50 para trabalhar
    ahhhhhhhhhhhhh

--- FrontEnd/paginaweb/js/script_index.js
+++ FrontEnd/paginaweb/js/script_index.js
@@ -14,11 +14,13 @@ btnMenu.addEventListener('click', toggleMenu);
 $(function() {
     $(window).on("scroll", function() {
         if ($(window).scrollTop() > 100) {
-            $(".nav-link").addClass("link-scroll-effect");
             $("nav").addClass("nav-scroll-effect");
+            $(".nav-link").addClass("link-scroll-effect");
+            $(".login-button").addClass("login-button-scroll-effect");
         } else {
-            $(".nav-link").removeClass("link-scroll-effect");
             $("nav").removeClass("nav-scroll-effect");
+            $(".nav-link").removeClass("link-scroll-effect");
+            $(".login-button").removeClass("login-button-scroll-effect");
         }
     });
 });
