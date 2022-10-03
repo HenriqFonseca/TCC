31c41a94c639e3a9d77624592033e52ed50154b8
Alexandre Vieira <101663403+ALEavds@users.noreply.github.com>
2022-10-03 20:40:35 -0300

    nsei

--- FrontEnd/paginaweb/index.php
+++ FrontEnd/paginaweb/index.php
@@ -12,11 +12,11 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
+   
     <!-- styles -->
-    <link href="./css/style.css" rel="stylesheet">
+    <link href="/paginaweb/css/style.css" rel="stylesheet">
     <!-- scripts -->
-    <script defer src="./js/script_index.js"></script>
+    <script defer src="/paginaweb/js/script_index.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <!-- pwa -->
     <link rel="manifest" href="./manifest.json" />
@@ -32,71 +32,8 @@
 <body>
     <header>
         <!----------NAV-BAR------------->
-        <nav class="nav-bar">
-            <div class="nav-logo-box">
-                <svg width="200" height="25.877456789880426" viewBox="0 0 350.00000000000006 52.00029776817928" c>
-                    <defs id="SvgjsDefs3341">
-                        <linearGradient id="SvgjsLinearGradient3346">
-                            <stop id="SvgjsStop3347" stop-color="#8f5e25" offset="0"></stop>
-                            <stop id="SvgjsStop3348" stop-color="#fbf4a1" offset="0.5"></stop>
-                            <stop id="SvgjsStop3349" stop-color="#8f5e25" offset="1"></stop>
-                        </linearGradient>
-                        <linearGradient id="SvgjsLinearGradient3350">
-                            <stop id="SvgjsStop3351" stop-color="#8f5e25" offset="0"></stop>
-                            <stop id="SvgjsStop3352" stop-color="#fbf4a1" offset="0.5"></stop>
-                            <stop id="SvgjsStop3353" stop-color="#8f5e25" offset="1"></stop>
-                        </linearGradient>
-                    </defs>
-                    <g id="SvgjsG3342" featurekey="33DNMO-0"
-                        transform="matrix(2.9055290123486506,0,0,2.9055290123486506,-3.9515194983580906,-18.885937194802032)"
-                        fill="url(#SvgjsLinearGradient3346)">
-                        <path
-                            d="M1.36 20 l0 -13.22 l3.36 0 l0 13.22 l-3.36 0 z M7.44 20 l0 -13.22 l5.08 0 c3.66 0 4.96 1.58 4.96 3.7 c0 1.98 -1.12 3.1 -2.34 3.68 c0.52 0.28 0.9 0.8 1.24 1.74 c0.28 0.8 0.42 1.56 1.86 1.5 l-0.58 2.72 c-3.16 0.4 -3.9 -1.64 -4.44 -3.1 c-0.56 -1.52 -0.78 -1.68 -1.9 -1.68 l-0.52 0 l0 4.66 l-3.36 0 z M12.440000000000001 9.6 l-1.64 0 l0 2.92 l1.64 0 c1.22 0 1.68 -0.76 1.68 -1.54 c0 -0.98 -0.56 -1.38 -1.68 -1.38 z M25.799999999999997 20.28 c-4.3 0 -6.52 -2.92 -6.52 -6.92 c0 -4.1 2.32 -6.86 6.64 -6.86 c4.08 0 6.54 2.46 6.54 6.7 c0 4.1 -2.32 7.08 -6.66 7.08 z M25.86 17.5 c1.88 0 3.06 -1.24 3.06 -4.16 c0 -2.88 -1.14 -4.06 -3.02 -4.06 c-1.94 0 -3.08 1.26 -3.08 4.1 c0 2.88 1.16 4.12 3.04 4.12 z M34.519999999999996 20 l0 -13.22 l3.26 0 l3.66 5.38 c0.54 0.8 1.24 1.9 1.88 2.96 c-0.06 -1.18 -0.1 -2.46 -0.1 -3.42 l0 -4.92 l3.2 0 l0 13.22 l-3.28 0 l-4.08 -5.96 c-0.4 -0.58 -0.94 -1.48 -1.44 -2.38 c0.06 1.08 0.1 2.18 0.1 2.94 l0 5.4 l-3.2 0 z M56.38 20 l-4.78 -13.22 l3.54 0 l2.28 6.4 c0.18 0.46 0.36 1.26 0.5 2.1 c0.18 -0.82 0.36 -1.64 0.54 -2.1 l1.36 -3.74 l-1.02 -2.66 l3.4 0 l2.32 6.4 c0.16 0.4 0.34 1.1 0.5 1.84 c0.14 -0.74 0.3 -1.42 0.46 -1.84 l2.16 -6.4 l3.26 0 l-4.52 13.22 l-2.68 0 l-2.38 -6.54 c-0.14 0.48 -0.28 0.94 -0.42 1.34 l-1.84 5.2 l-2.68 0 z M78.12 20.28 c-4.3 0 -6.52 -2.92 -6.52 -6.92 c0 -4.1 2.32 -6.86 6.64 -6.86 c4.08 0 6.54 2.46 6.54 6.7 c0 4.1 -2.32 7.08 -6.66 7.08 z M78.18 17.5 c1.88 0 3.06 -1.24 3.06 -4.16 c0 -2.88 -1.14 -4.06 -3.02 -4.06 c-1.94 0 -3.08 1.26 -3.08 4.1 c0 2.88 1.16 4.12 3.04 4.12 z M86.84 20 l0 -13.22 l5.08 0 c3.66 0 4.96 1.58 4.96 3.7 c0 1.98 -1.12 3.1 -2.34 3.68 c0.52 0.28 0.9 0.8 1.24 1.74 c0.28 0.8 0.42 1.56 1.86 1.5 l-0.58 2.72 c-3.16 0.4 -3.9 -1.64 -4.44 -3.1 c-0.56 -1.52 -0.78 -1.68 -1.9 -1.68 l-0.52 0 l0 4.66 l-3.36 0 z M91.84 9.6 l-1.64 0 l0 2.92 l1.64 0 c1.22 0 1.68 -0.76 1.68 -1.54 c0 -0.98 -0.56 -1.38 -1.68 -1.38 z M99.34 20 l0 -13.22 l3.36 0 l0 4.94 l0.6 0 l3.42 -4.94 l3.92 0 l-4.5 6.34 l4.68 6.88 l-4.04 0 l-3.5 -5.26 l-0.58 0 l0 5.26 l-3.36 0 z M111.28000000000002 16.34 l3.3 -0.68 c0.02 1.4 1.1 1.84 2.2 1.84 c1.04 0 1.64 -0.38 1.64 -1 c0 -2.1 -6.78 -1.14 -6.78 -5.94 c0 -2.64 2.04 -4.06 5.1 -4.06 c3.08 0 5.02 1.46 4.92 3.58 l-3.3 0.68 c-0.02 -0.54 -0.14 -1.48 -1.7 -1.48 c-1.08 0 -1.62 0.44 -1.62 1.04 c0 2.18 6.78 1.04 6.78 5.82 c0 2.4 -1.7 4.14 -5.08 4.14 c-3.6 0 -5.58 -1.96 -5.46 -3.94 z">
-                        </path>
-                    </g>
-
-                </svg>
-            </div>
-            <button id="btn-menu">
-                <img src="./img/outline_menu_white_48dp.png" alt="">
-            </button>
-            <div id="nav-item-box">
-                <ul class="nav-list">
-                    <li class="item-box">
-                        <a class="nav-link active-item" href="">item
-                        </a>
-                    </li>
-                    <li class="item-box sub-menu-box">
-                        <a class="nav-link" href="">item
-
-                        </a>
-                        <!----------SUB-MENU------------->
-                        <!-- <ul class="sub-menu">
-                            <li class="sub-menu-item">
-                                <a href="" class="sub-menu-link">sub-item </a>
-                            </li>
-                            <li class="sub-menu-item">
-                                <a href="" class="sub-menu-link">sub-item </a>
-                            </li>
-                            <li class="sub-menu-item">
-                                <a href="" class="sub-menu-link">sub-item </a>
-                            </li>
-                        </ul> -->
-                    </li>
-                </ul>
-                <button class="login-button">
-                    Fazer Login
-                </button>
-
-            </div>
-        </nav>
+ <?php include'' ?>
     </header>
-
-    <!-- <img src="./img/8225.jpg" style="width: 100%;" alt=""> -->
-
-
-
-
     <main>
         <!--AQUI EU VOU TESTAR ALGUMAS FORMAS DE CRIAR UM CARROSSEL-->
         <!-- Carousel -->
@@ -112,13 +49,13 @@
             <!-- The slideshow/carousel -->
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="./img/8225.jpg" alt="Los Angeles" class="d-block w-100">
+                    <img src="/paginaweb/img/8225.jpg" alt="Los Angeles" class="d-block w-100">
                 </div>
                 <div class="carousel-item">
-                    <img src="./img/897.jpg" alt="Chicago" class="d-block w-100">
+                    <img src="/paginaweb/img/897.jpg" alt="Chicago" class="d-block w-100">
                 </div>
                 <div class="carousel-item">
-                    <img src="./img/lulaxbolso.jpg" alt="New York" class="d-block w-100">
+                    <img src="/paginaweb/img/lulaxbolso.jpg" alt="New York" class="d-block w-100">
                 </div>
             </div>
 
@@ -137,7 +74,7 @@
             <div class="btn-row">
                 <div class="btn-box">
                     <button class="btn-small">
-                        <img src="./img/dumbbell.png" alt="">
+                        <img src="/paginaweb/img/dumbbell.png" alt="">
                     </button>
                     <div class="btn-caption">
                         <h3>Algum Texto</h3>
@@ -145,7 +82,7 @@
                 </div>
                 <div class="btn-box">
                     <button class="btn-small">
-                        <img src="./img/sportsman.png" alt="">
+                        <img src="/paginaweb/img/sportsman.png" alt="">
                     </button>
                     <div class="btn-caption">
                         <h3>Algum Texto</h3>
@@ -153,7 +90,7 @@
                 </div>
                 <div class="btn-box">
                     <button class="btn-small">
-                        <img src="./img/location.png" alt="">
+                        <img src="/paginaweb/img/location.png" alt="">
                     </button>
                     <div class="btn-caption">
                         <h3>Algum Texto</h3>
@@ -163,7 +100,7 @@
             <div class="btn-row">
                 <div class="btn-box">
                     <button class="btn-medium">
-                        <img src="./img/chat.png" alt="">
+                        <img src="/paginaweb/img/chat.png" alt="">
                     </button>
                     <div class="btn-caption">
                         <h3>Algum Texto</h3>
@@ -171,7 +108,7 @@
                 </div>
                 <div class="btn-box">
                     <button class="btn-medium">
-                        <img src="./img/invoice.png" alt="">
+                        <img src="/paginaweb/img/invoice.png" alt="">
                     </button>
                     <div class="btn-caption">
                         <h3>Algum Texto</h3>
@@ -181,7 +118,7 @@
             <div class="btn-row">
                 <div class="btn-box">
                     <button class="btn-large">
-                        <img src="./img/calendar.png" alt="">
+                        <img src="/paginaweb/img/calendar.png" alt="">
                     </button>
                     <div class="btn-caption">
                         <h3>Algum Texto</h3>
@@ -189,7 +126,7 @@
                 </div>
                 <div class="btn-box">
                     <button class="btn-large">
-                        <img src="./img/note.png" alt="">
+                        <img src="/paginaweb/img/note.png" alt="">
                     </button>
                     <div class="btn-caption">
                         <h3>Algum Texto</h3>
---
+++ FrontEnd/telaLogin/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <div class="cadastro">
+                <form class="form-signin">
+                    <h2 class="form-signin-heading">Insira seu cadastro</h2>
+                    <input type="text" class="input" name="username" placeholder="CPF" required="" autofocus="" />
+                    <br>
+                    <input type="password" class="input" name="password" placeholder="Senha" required="" />
+                    <label class="checkbox">
+            <br>
+            <button class="login-button">
+              Fazer Login
+          </button><br>
+        </form>
+        <ul class="nav-item-box">
+          <a class="forgot-password" href="">Esqueci minha senha
+          </a>
+      </div>
+      <div class="logo-box">
+        <a href="/paginaweb/index.html"><img class="img" src="./img/logo-vertical.png" alt=""></a>
+      </div>
+        </div><div class="drops">
+        <div class="drop drop-1"></div>
+        <div class="drop drop-2"></div>
+        <div class="drop drop-3"></div>
+        <div class="drop drop-4"></div>
+        <div class="drop drop-5"></div>
+      </div>
+    </div>
+</body>
