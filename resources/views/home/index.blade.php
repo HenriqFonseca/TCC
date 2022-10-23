16791749bc342f021d785d5501d2cc6278367ef4
Henriq_Fonseca <henrique12095@gmail.com>
2022-10-22 22:48:56 -0300

    alterei o ref manifest

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -29,7 +29,7 @@
     <!-- PWA  -->
     <meta name="theme-color" content="#6777ef"/>
     <link rel="apple-touch-icon" href="{{ asset('/img/logo.png') }}">
-    <link rel="manifest" href="{{ asset('/manifest.json') }}">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
 
     <title>IronWorks</title>
 </head>
