97fb25393a9f7a749a30282708a849ea03cd483c
Alexandre Vieira <101663403+ALEavds@users.noreply.github.com>
2022-10-24 22:00:13 -0300

    FOOTER COISADO

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -130,30 +130,6 @@
         <div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></div>
 
     </main>
-
-
-    <footer>
-        <div class="nav-footer">
-            <nav>
-                <h2>Navegue no site</h2>
-                <ul>
-                    <li><a href="#">Capa</a></li>
-                    <li><a href="#">História dos instrumentos de metal</a></li>
-                    <li><a href="#">Seja um colaborarador</a></li>
-                    <li><a href="#">Quem somos</a></li>
-                </ul>
-            </nav>
-            <nav>
-                <h2>Fique conectado !</h2>
-                <ul>
-                    <li><a href="#">Email</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Youtube</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ul>
-            </nav>
-        </div>
-        <small>IronWorks Copyright © 2077 </small>
     </footer>
 
     {{-- Script Service Worker --}}
