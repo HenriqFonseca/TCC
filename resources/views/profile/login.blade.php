c523e9622d63a0b3c098c2bcfc3bcb8fac9af293
Alexandre Vieira <alxndr331@gmail.com>
2022-11-09 21:29:36 -0300

    Revert "sim"
    This reverts commit 94dca4059192afcfe1d6534aa092ff443c8e1d5a.

--- resources/views/profile/login.blade.php
+++ resources/views/profile/login.blade.php
@@ -12,7 +12,6 @@
 </head>
 
 <body>
-<<<<<<< Updated upstream
     <div class="container">
         <div class="wrapper">
             @if ($errors->any())
@@ -56,44 +55,6 @@
             <div class="drop drop-4"></div>
             <div class="drop drop-5"></div>
         </div>
-=======
-  <div class="container">
-    <div class="wrapper">
-      <div class="cadastro">
-        <form class="form-signin" method="POST" action="login">
-          @csrf
-          <h2 class="form-signin-heading">Insira seu cadastro</h2>
-          <input type="text" class="input" name="cpf" onkeypress="$(this).mask('000.000.000-00')" autocomplete="off" maxlength="14"  placeholder="CPF"/>
-          @error('cpf')
-            <div class="erro">{{ $message }}</div>
-          @enderror
-          <br>
-          <input type="password" class="input" name="password" placeholder="Senha"/>
-          @error('password')
-            <div class="erro">{{ $message }}</div>
-          @enderror
-          <div class="login">
-            <br>
-            <button class="login-button">
-              Fazer Login
-            </button>
-            <br>
-          </div>
-          
-        </form>
-          <a class="forgot-password" href="">Esqueci minha senha</a>
-
-      </div>
-      <a href="{{ route('home.index') }}" class="logo-box">
-        <img class="img" src="img/logo-vertical.png" alt="">
-      </a>
-    </div>
-    <div class="drops">
-      <div class="drop drop-1"></div>
-      <div class="drop drop-2"></div>
-      <div class="drop drop-3"></div>
-      <div class="drop drop-4"></div>
->>>>>>> Stashed changes
     </div>
 
 
