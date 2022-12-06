426c317fe7066b887d809f217eacd4c2300d48e7
Alexandre Vieira <alxndr331@gmail.com>
2022-12-06 01:38:50 -0300

    estilizando a lista de alunos

--- resources/views/treinos/listaalunos.blade.php
+++ resources/views/treinos/listaalunos.blade.php
@@ -40,32 +40,42 @@
                        
                         <form class="form-search" action="/treinos" method="GET">
                                 <input type="text" placeholder="Pesquisar..." name="search"  id="search">
+                                <button class="search-button" type="submit"><img src="img/person_search.png" alt=""></button>
                             </form>
                         
                             <div class="content">
                                 
                                 <!-- <h2>Buscando por: {{ $search }}</h2> -->
                                 @foreach ($aluno as $aluno)
-                                    <a class="list-content" href="{{ url('/treinos/create/' . $aluno->id) }}">
-                                        <div >
-                                            <div >
+                                    <a class="link-content" href="{{ url('/treinos/create/' . $aluno->id) }}">
+                                        <div class="aluno-container" >
                                                 <div >
                                                     <img src="/img/toduro.jpg" >
                                                 </div>
-                                                <div style="background-color: black;" >
+                                                <div class="dados-container" >
                                                     @if ($aluno->status_treino == 0)
-                                                        <h2>{{ $aluno->nome }} {{ $aluno->sobrenome }} Data
-                                                            de Nascimento:{{ date('d/m/Y', strtotime($aluno->dataNascimento)) }} idade:{{ $aluno->dataNascimento->age }} SEM TREINO</h2>
+                                                        <div>
+                                                            <h2>{{ $aluno->nome }} {{ $aluno->sobrenome }}</h2>
+                                                                 <h2>Data de Nasc:{{ date('d/m/Y', strtotime($aluno->dataNascimento)) }} </h2>
+                                                                 <h2> Idade:{{ $aluno->dataNascimento->age }}</h2> 
+                                                        </div>
+                                                        <button class="sem-treino">
+                                                            <h2>SEM TREINO</h2>
+                                                        </button>
                                                         @foreach ($aluno->treinos as $item)
                                                             {{ $item->nome }}
                                                         @endforeach
                                                     @else
-                                                        <h2>{{ $aluno->nome }} {{ $aluno->sobrenome }} Data de
-                                                            Nascimento:{{ date('d/m/Y', strtotime($aluno->dataNascimento)) }}
-                                                            idade:{{ $aluno->dataNascimento->age }} COM TREINO</h2>
+                                                        <div>
+                                                            <h2>{{ $aluno->nome }} {{ $aluno->sobrenome }}</h2>
+                                                            <h2>Data de Nasc:{{ date('d/m/Y', strtotime($aluno->dataNascimento)) }}</h2>
+                                                            <h2>idade:{{ $aluno->dataNascimento->age }}</h2>
+                                                        </div>
+                                                             <button class="com-treino">
+                                                                 <h2 class="com-treino">COM TREINO</h2>
+                                                             </button>
                                                     @endif
                                                 </div>
-                                            </div>
                                         </div>
                                     </a>
                             
