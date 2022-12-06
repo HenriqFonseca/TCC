d6323d3fa8c649ebc05473f3223702e06b159fb3
Henriq_Fonseca <henrique12095@gmail.com>
2022-12-06 00:23:49 -0300

    erros e validações login register

--- resources/views/profile/register_professor.blade.php
+++ resources/views/profile/register_professor.blade.php
@@ -13,19 +13,7 @@
 
 <body>
 
-    <div class="alert-container" style="max-width:500px ;">
-        @if ($errors->any())
-        <div class="alert" style="max-width: 500px; position: absolute; top:5vh; width:27rem;">
-            <ul>
-                <li style="list-style: none">
-                    @foreach ($errors->all() as $error)
-                    <p>{{ $error }}</p>
-                    @endforeach
-                </li>
-            </ul>
-        </div>
-        @endif
-    </div>
+   
     <div class="container">
         <div class="wrapper">
 
@@ -39,58 +27,80 @@
                     <br>
                     <div class="nome">
                         {{-- CASO HAJA UM ERRO NO INPUT NOME A CLASSE VAI MUDAR --}}
-                        <input type="text" @if ($errors->has('nome'))
-                        class="input-error"
-                        @else
-                        class="input"
-                        @endif
-                        name="nome" placeholder="Nome" />
+                        <div class="container-erro-nome">
+                            <input type="text" @if ($errors->has('nome'))
+                            class="input-error" name="nome" placeholder="Nome" />
+                            <small style="color: red; background-color: white; text-stroke:;">@error('nome') {{ $message }} @enderror</small>
+                            @else
+                            class="input" name="nome" placeholder="Nome" />
+                            @endif
+                        </div>
 
                         {{-- CASO HAJA UM ERRO NO INPUT SOBRENOME A CLASSE VAI MUDAR --}}
-                        <input type="text" @if ($errors->has('sobrenome'))
-                        class="input-error"
-                        @else class="input"
-                        @endif
-                        name="sobrenome" placeholder="Sobrenome" />
+                        <div class="container-erro">
+
+                            <input type="text" @if ($errors->has('sobrenome'))
+                            class="input-error" name="sobrenome" placeholder="Sobrenome" />
+                            <small style="color: red; background-color: white;">@error('sobrenome') {{ $message }} @enderror</small>
+                            @else class="input" name="sobrenome" placeholder="Sobrenome" />
+                            @endif
+                        </div>
                     </div>
                     <br>
                     {{-- CASO HAJA UM ERRO NO INPUT EMAIL A CLASSE VAI MUDAR --}}
-                    <input type="text" @if ($errors->has('email'))
-                    class="input-error"
-                    @else class="input"
-                    @endif name="email" id="email" autocomplete="off"
-                    placeholder="Email" />
+                    <div class="container-erro" style="width: 100%">
+                        <div class="container-erro">
+                        <input type="text" @if ($errors->has('email'))
+                        class="input-error" name="email" id="email" autocomplete="off"
+                        placeholder="Email" />
+                        <small style="color: red; background-color: white; text-align:left;">@error('email') {{ $message }} @enderror</small>
+                        @else class="input" name="email" id="email" autocomplete="off" placeholder="Email" />
+                        @endif 
+                        </div>
+                    </div>
                     <br>
-                    <input type="date" @if ($errors->has('dataNascimento'))
-                    class="input-error"
-                    @else class="input"
-                    @endif name="dataNascimento" id="dataNascimento" autocomplete="off"
-                    placeholder="" />
+                    <div class="container-erro" style="width: 100%">
+                        <div class="container-erro">
+                            <input type="date" @if ($errors->has('dataNascimento'))
+                            class="input-error" name="dataNascimento" id="dataNascimento" autocomplete="off" placeholder="" />
+                            <small style="color: red; background-color: white; text-align:left;">@error('dataNascimento') {{ $message }} @enderror</small>
+                            @else class="input" name="dataNascimento" id="dataNascimento" autocomplete="off" placeholder="" />
+                            @endif 
+                        </div>
+                    </div>
                     <br>
 
-                    <input type="text" placeholder="RG" @if ($errors->has('rg'))
-                    class="input-error"
-                    @else class="input"
-                    @endif
-                    name="rg" id="rg" onkeypress="$(this).mask('00.000.000-0')" autocomplete="off" maxlength="12" placeholder="RG" />
+                    <div class="container-erro" style="width: 100%">
+                        <div class="container-erro">
+                            <input type="text" placeholder="RG" @if ($errors->has('rg'))
+                            class="input-error"  name="rg" id="rg" onkeypress="$(this).mask('00-000-000-0')" autocomplete="off" maxlength="9" placeholder="RG" />
+                            <small style="color: red; background-color: white; text-align:left;"> @error('rg') {{ $message }} @enderror </small>
+                            @else class="input"  name="rg" id="rg" onkeypress="$(this).mask('00-000-000-0')" autocomplete="off" maxlength="9" placeholder="RG" />
+                            @endif
+                        </div>        
+                    </div>        
                     <br>
                     {{-- CASO HAJA UM ERRO NO INPUT CPF A CLASSE VAI MUDAR --}}
-                    <input type="text" @if ($errors->has('cpf'))
-                    class="input-error"
-                    @else class="input"
-                    @endif
-                    name="cpf" id="cpf"onkeypress="$(this).mask('000.000.000-00')" autocomplete="off" maxlength="14" placeholder="CPF" />
+                    <div class="container-erro" style="width: 100%">
+                        <div class="container-erro">
+                            <input type="text" @if ($errors->has('cpf'))
+                            class="input-error" name="cpf" id="cpf" onkeypress="$(this).mask('000.000.000-00')" autocomplete="off" maxlength="14" placeholder="CPF" />
+                            <small style="color: red; background-color: white; text-align:left;">@error('cpf') {{ $message }} @enderror</small>
+                            @else class="input" name="cpf" id="cpf" onkeypress="$(this).mask('000.000.000-00')" autocomplete="off" maxlength="14" placeholder="CPF" />
+                            @endif
+                        </div>
+                    </div>
                     <br>
                     {{-- CASO HAJA UM ERRO NO INPUT PASSWORD A CLASSE VAI MUDAR --}}
-                    <input type="password" @if ($errors->has('password'))
-                    class="input-error"
-                    @else class="input"
-                    @endif name="password" placeholder="Senha" />
-
-                    <br>
-                    <button type="submit" class="login-button">
-                        Registrar
-                    </button>
+                    <div class="container-erro" style="width: 100%">
+                        <div class="container-erro">
+                            <input type="password" @if ($errors->has('password'))
+                            class="input-error" name="password" placeholder="Senha" />
+                            <small style="color: red; background-color: white; text-align:left;">@error('password') {{ $message }} @enderror</small>
+                            @else class="input" name="password" placeholder="Senha" />
+                            @endif 
+                        </div>
+                    </div>
 
 
 
