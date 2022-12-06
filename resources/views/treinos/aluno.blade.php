a9667506150b20d1b9c0918909ad1e2e9df7907b
Henriq_Fonseca <henrique12095@gmail.com>
2022-12-06 03:39:25 -0300

    imagem nova e tela treino aluno

--- resources/views/treinos/aluno.blade.php
+++ resources/views/treinos/aluno.blade.php
@@ -39,38 +39,23 @@
     @extends('navbar-pronta.nav-bar')
     @section('conteudo')
         <div class="container-md" style="transform: translateY(5rem) !important;">
-            <div class="container-user" style="background-color:blue;">
-                <div class="text" style="background-color: red;">
-                    <div class="img-container" style="">
-                        <img src="/img/perfil/{{ auth()->user()->imagePerfil }}" class="img-perfil" alt="">
-                        <p>{{ auth()->user()->nome }} {{ auth()->user()->sobrenome }}</p>
-                    </div>
-
-                </div>
-                <div class="wrapper">
-
-                    <div class="info-user">
-
-                    </div>
-                    <div class="text-user">
-
-                    </div>
-                </div>
-
+            <div class="content">
+               
+                <form action="/perfil/{{ $user->id }}" method="POST" enctype="multipart/form-data">
+                    @method('PUT')
+                    @csrf
+                    <label for="imagePerfil">Imagem de Perfil</label>
+                    <input type="file" name="imagePerfil" id="imagePerfil">
+                    <button type="submit">Definir Foto</button>
+                </form>
             </div>
-            <form action="/perfil/{{ $user->id }}" method="POST" enctype="multipart/form-data">
-                @method('PUT')
-                @csrf
-                <label for="imagePerfil">Imagem de Perfil</label>
-                <input type="file" name="imagePerfil" id="imagePerfil">
-                <button type="submit">Definir Foto</button>
-            </form>
             <div class="row">
                 <div class="col-12">
                     <div class="container-md">
                         <div class="row">
                             <div class="col-12">
                                 <h2>
+                                    <div class="content">
                                     Treino A<br>
                                     @foreach ($user->treinos as $item)
                                         @if ($item->tipoTreino == 'treino_a')
@@ -81,8 +66,10 @@
                                             @endforeach
                                         @endif
                                     @endforeach
+                                    </div>
                                     <br><br>
 
+                                    <div class="content">
                                     TREINO B<br>
                                     @foreach ($user->treinos as $item)
                                         @if ($item->tipoTreino == 'treino_b')
@@ -93,8 +80,10 @@
                                             @endforeach
                                         @endif
                                     @endforeach
+                                    </div>
                                     <BR><BR>
 
+                                    <div class="content">
                                     TREINO C <br>
                                     @foreach ($user->treinos as $item)
                                         @if ($item->tipoTreino == 'treino_c')
@@ -105,8 +94,10 @@
                                             @endforeach
                                         @endif
                                     @endforeach
+                                    </div>
                                     <BR><BR>
 
+                                        <div class="content">
                                     TREINO D
                                     <br>
                                     @foreach ($user->treinos as $item)
