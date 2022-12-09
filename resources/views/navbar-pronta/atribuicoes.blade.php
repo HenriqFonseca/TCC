007ad409c25d0d4d6315bb1b82700bd62b42b736
Alexandre Vieira <alxndr331@gmail.com>
2022-12-09 19:56:40 -0300

    responsividade das atribuicoes

--- resources/views/navbar-pronta/atribuicoes.blade.php
+++ resources/views/navbar-pronta/atribuicoes.blade.php
@@ -26,13 +26,14 @@
         }
 
         .container {
-            margin-top: 6rem;
+            margin: 6rem 0 6rem 0;
             width: 100%;
             max-width: 1300px;
             display: flex;
             flex-direction: column;
             gap: 1rem;
             background-color: var(--footer-color);
+            backdrop-filter: blur(10px);
             padding: 2rem;
             border-radius: 0.7rem;
         }
@@ -44,8 +45,12 @@
             border-radius: 0.7rem;
         }
 
-        .link-attrib{
+        .link-attrib {
             color: black;
+        }
+
+        .link-attrib>span {
+            width: 100%;
         }
 
         .link-attrib:hover {
@@ -57,23 +62,32 @@
     @section('conteudo')
 
     <div class="container">
-        <a href="https://www.flaticon.com/free-icons/dumbbell" class="link-attrib"><span>https://www.flaticon.com/free-icons/dumbbell</span></a>
-        <a href="https://www.flaticon.com/free-icons/invoice" class="link-attrib"><span>https://www.flaticon.com/free-icons/invoice</span></a>
-        <a href="https://www.flaticon.com/free-icons/architecture-and-city" class="link-attrib"><span>https://www.flaticon.com/free-icons/architecture-and-city</span></a>
-        <a href="https://www.flaticon.com/free-icons/gym" class="link-attrib"><span>https://www.flaticon.com/free-icons/gym</span></a>
-        <a href="https://www.flaticon.com/free-icons/customers" class="link-attrib"><span>https://www.flaticon.com/free-icons/customers</span></a>
-        <a href="https://www.pexels.com/pt-br/foto/homem-segurando-haltere-preto-1229356/" class="link-attrib"><span>https://www.pexels.com/pt-br/foto/homem-segurando-haltere-preto-1229356/</span></a>
-        <a href="https://unsplash.com/@scottwebb?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="link-attrib"><span>https://unsplash.com/@scottwebb?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText</span></a>
-        <a href="https://www.flaticon.com/br/icones-gratis/academia" class="link-attrib"><span>https://www.flaticon.com/br/icones-gratis/academia</span></a>
+        <a href="https://www.flaticon.com/free-icons/dumbbell" class="link-attrib">
+            <span>https://www.flaticon.com/free-icons/dumbbell</span>
+        </a>
+        <a href="https://www.flaticon.com/free-icons/invoice" class="link-attrib">
+            <span>https://www.flaticon.com/free-icons/invoice</span>
+        </a>
+        <a href="https://www.flaticon.com/free-icons/architecture-and-city" class="link-attrib">
+            <span>https://www.flaticon.com/free-icons/architecture-and-city</span>
+        </a>
+        <a href="https://www.flaticon.com/free-icons/gym" class="link-attrib">
+            <span>https://www.flaticon.com/free-icons/gym</span>
+        </a>
+        <a href="https://www.flaticon.com/free-icons/customers" class="link-attrib">
+            <span>https://www.flaticon.com/free-icons/customers</span>
+        </a>
+        <a href="https://www.pexels.com/pt-br/foto/homem-segurando-haltere-preto-1229356/" class="link-attrib">
+            <span>https://www.pexels.com/pt-br/foto/homem-segurando-haltere-preto-1229356/</span>
+        </a>
+        <a href="https://unsplash.com/@scottwebb" class="link-attrib">
+            <span>https://unsplash.com/@scottwebb</span>
+        </a>
+        <a href="https://www.flaticon.com/br/icones-gratis/academia" class="link-attrib">
+            <span>https://www.flaticon.com/br/icones-gratis/academia</span>
+        </a>
 
-        <!-- <div class="link-attrib"><a href="" title="dumbbell icons">Dumbbell icons created by Icongeek26 - Flaticon</a></div>
-        <div class="link-attrib"><a href="" title="invoice icons">Invoice icons created by Icongeek26 - Flaticon</a></div>
-        <div class="link-attrib"><a href="" title="architecture and city icons">Architecture and city icons created by Icongeek26 - Flaticon</a></div>
-        <div class="link-attrib"><a href="" title="gym icons">Gym icons created by Icongeek26 - Flaticon</a></div>
-        <div class="link-attrib"><a href="" title="customers icons">Customers icons created by Eucalyp - Flaticon</a></div>
-        <div class="link-attrib"><a href="/"> Anush Gorak pexels</a></div>
-        <div class="link-attrib"><a href="">Scottwebb</a></div>
-        <div class="link-attrib"><a href="https://www.flaticon.com/br/icones-gratis/academia" title="academia ícones">Academia ícones criados por Freepik - Flaticon</a></div> -->
+
     </div>
 
     @endsection
