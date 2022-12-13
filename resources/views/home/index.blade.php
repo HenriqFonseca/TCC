b0429e24bed95e2b3ef27598bbb222788967719e
Henriq_Fonseca <henrique12095@gmail.com>
2022-12-12 21:38:31 -0300

    card title mobile

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -317,7 +317,7 @@
                         }
 
                         .text-cards h1 {
-                            font-size: 50px;
+                            font-size: 60px !important;
                             position: absolute;
                             margin: auto;
                             right: 0;
